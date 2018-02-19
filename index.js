509295743fe8a74f6967a559cf519c80747c10cb
Aubree Abril <aubree.abril@gmail.com>
2018-02-19 20:24:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`;
+=======
+var conversation = `Let's talk about ${topic}`;
+>>>>>>> fb9e700c3714e99c3ef34e20167c0f697d0ab20b
